ee2a592496e9faba18cbafd37d8276ca449196ad
lamthun <lilei420984@163.com>
2023-03-30 09:10:36 +0800

    添加自己的内容

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -5,18 +5,17 @@ import sys
 
 import gradio as gr
 
-from modules.utils import *
-from modules.presets import *
-from modules.overwrites import *
-from modules.chat_func import *
-from modules.openai_func import get_usage
+from utils import *
+from presets import *
+from overwrites import *
+from chat_func import *
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
 )
 
-my_api_key = ""  # 在这里输入你的 API 密钥
+my_api_key = "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"  # 在这里输入你的 API 密钥
 
 # if we are running in Docker
 if os.environ.get("dockerrun") == "yes":
@@ -25,7 +24,6 @@ else:
     dockerflag = False
 
 authflag = False
-auth_list = []
 
 if dockerflag:
     my_api_key = os.environ.get("my_api_key")
@@ -36,7 +34,6 @@ if dockerflag:
     username = os.environ.get("USERNAME")
     password = os.environ.get("PASSWORD")
     if not (isinstance(username, type(None)) or isinstance(password, type(None))):
-        auth_list.append((os.environ.get("USERNAME"), os.environ.get("PASSWORD")))
         authflag = True
 else:
     if (
@@ -47,82 +44,132 @@ else:
         with open("api_key.txt", "r") as f:
             my_api_key = f.read().strip()
     if os.path.exists("auth.json"):
-        authflag = True
-        with open("auth.json", "r", encoding='utf-8') as f:
+        with open("auth.json", "r") as f:
             auth = json.load(f)
-            for _ in auth:
-                if auth[_]["username"] and auth[_]["password"]:
-                    auth_list.append((auth[_]["username"], auth[_]["password"]))
-                else:
-                    logging.error("请检查auth.json文件中的用户名和密码！")
-                    sys.exit(1)
+            username = auth["username"]
+            password = auth["password"]
+            if username != "" and password != "":
+                authflag = True
 
 gr.Chatbot.postprocess = postprocess
 PromptHelper.compact_text_chunks = compact_text_chunks
 
-with open("assets/custom.css", "r", encoding="utf-8") as f:
+with open("custom.css", "r", encoding="utf-8") as f:
     customCSS = f.read()
 
-with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
+with gr.Blocks(
+    css=customCSS,
+    theme=gr.themes.Soft(
+        primary_hue=gr.themes.Color(
+            c50="#02C160",
+            c100="rgba(2, 193, 96, 0.2)",
+            c200="#02C160",
+            c300="rgba(2, 193, 96, 0.32)",
+            c400="rgba(2, 193, 96, 0.32)",
+            c500="rgba(2, 193, 96, 1.0)",
+            c600="rgba(2, 193, 96, 1.0)",
+            c700="rgba(2, 193, 96, 0.32)",
+            c800="rgba(2, 193, 96, 0.32)",
+            c900="#02C160",
+            c950="#02C160",
+        ),
+        secondary_hue=gr.themes.Color(
+            c50="#576b95",
+            c100="#576b95",
+            c200="#576b95",
+            c300="#576b95",
+            c400="#576b95",
+            c500="#576b95",
+            c600="#576b95",
+            c700="#576b95",
+            c800="#576b95",
+            c900="#576b95",
+            c950="#576b95",
+        ),
+        neutral_hue=gr.themes.Color(
+            name="gray",
+            c50="#f9fafb",
+            c100="#f3f4f6",
+            c200="#e5e7eb",
+            c300="#d1d5db",
+            c400="#B2B2B2",
+            c500="#808080",
+            c600="#636363",
+            c700="#515151",
+            c800="#393939",
+            c900="#272727",
+            c950="#171717",
+        ),
+        radius_size=gr.themes.sizes.radius_sm,
+    ).set(
+        button_primary_background_fill="#06AE56",
+        button_primary_background_fill_dark="#06AE56",
+        button_primary_background_fill_hover="#07C863",
+        button_primary_border_color="#06AE56",
+        button_primary_border_color_dark="#06AE56",
+        button_primary_text_color="#FFFFFF",
+        button_primary_text_color_dark="#FFFFFF",
+        button_secondary_background_fill="#F2F2F2",
+        button_secondary_background_fill_dark="#2B2B2B",
+        button_secondary_text_color="#393939",
+        button_secondary_text_color_dark="#FFFFFF",
+        # background_fill_primary="#F7F7F7",
+        # background_fill_primary_dark="#1F1F1F",
+        block_title_text_color="*primary_500",
+        block_title_background_fill = "*primary_100",
+        input_background_fill="#F6F6F6",
+    ),
+) as demo:
     history = gr.State([])
     token_count = gr.State([])
     promptTemplates = gr.State(load_template(get_template_names(plain=True)[0], mode=2))
     user_api_key = gr.State(my_api_key)
-    user_question = gr.State("")
-    outputing = gr.State(False)
+    TRUECOMSTANT = gr.State(True)
+    FALSECONSTANT = gr.State(False)
     topic = gr.State("未命名对话历史记录")
 
     with gr.Row():
         gr.HTML(title)
         status_display = gr.Markdown(get_geoip(), elem_id="status_display")
 
-    with gr.Row().style(equal_height=True):
+    with gr.Row(scale=1).style(equal_height=True):
         with gr.Column(scale=5):
-            with gr.Row():
+            with gr.Row(scale=1):
                 chatbot = gr.Chatbot(elem_id="chuanhu_chatbot").style(height="100%")
-            with gr.Row():
+            with gr.Row(scale=1):
                 with gr.Column(scale=12):
                     user_input = gr.Textbox(
                         show_label=False, placeholder="在这里输入"
                     ).style(container=False)
                 with gr.Column(min_width=70, scale=1):
                     submitBtn = gr.Button("发送", variant="primary")
-                    cancelBtn = gr.Button("取消", variant="secondary", visible=False)
-            with gr.Row():
+            with gr.Row(scale=1):
                 emptyBtn = gr.Button(
                     "🧹 新的对话",
                 )
                 retryBtn = gr.Button("🔄 重新生成")
-                delFirstBtn = gr.Button("🗑️ 删除最旧对话")
-                delLastBtn = gr.Button("🗑️ 删除最新对话")
+                delLastBtn = gr.Button("🗑️ 删除一条对话")
                 reduceTokenBtn = gr.Button("♻️ 总结对话")
 
         with gr.Column():
             with gr.Column(min_width=50, scale=1):
                 with gr.Tab(label="ChatGPT"):
-                    keyTxt = gr.Textbox(
-                        show_label=True,
-                        placeholder=f"OpenAI API-key...",
-                        value=hide_middle_chars(my_api_key),
-                        type="password",
-                        visible=not HIDE_MY_KEY,
-                        label="API-Key",
-                    )
-                    usageTxt = gr.Markdown("**发送消息** 或 **提交key** 以显示额度", elem_id="usage_display")
-                    model_select_dropdown = gr.Dropdown(
-                        label="选择模型", choices=MODELS, multiselect=False, value=MODELS[0]
-                    )
+                    #keyTxt = gr.Textbox(
+                    #    show_label=True,
+                    #    placeholder=f"OpenAI API-key...",
+                    #    value=hide_middle_chars(my_api_key),
+                    #    type="password",
+                    #    visible=not HIDE_MY_KEY,
+                    #    label="API-Key",
+                    #)
+                    #model_select_dropdown = gr.Dropdown(
+                    #    label="选择模型", choices=MODELS, multiselect=False, value=MODELS[0]
+                    #)
                     use_streaming_checkbox = gr.Checkbox(
                         label="实时传输回答", value=True, visible=enable_streaming_option
                     )
-                    use_websearch_checkbox = gr.Checkbox(label="使用在线搜索", value=False)
-                    language_select_dropdown = gr.Dropdown(
-                        label="选择回复语言（针对搜索&索引功能）",
-                        choices=REPLY_LANGUAGES,
-                        multiselect=False,
-                        value=REPLY_LANGUAGES[0],
-                    )
-                    index_files = gr.Files(label="上传索引文件", type="file", multiple=True)
+                    #use_websearch_checkbox = gr.Checkbox(label="使用在线搜索", value=False)
+                    #index_files = gr.Files(label="上传索引文件", type="file", multiple=True)
 
                 with gr.Tab(label="Prompt"):
                     systemPromptTxt = gr.Textbox(
@@ -131,7 +178,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                         label="System prompt",
                         value=initial_prompt,
                         lines=10,
-                    ).style(container=False)
+                    ).style(container=True)
                     with gr.Accordion(label="加载Prompt模板", open=True):
                         with gr.Column():
                             with gr.Row():
@@ -141,7 +188,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                                         choices=get_template_names(plain=True),
                                         multiselect=False,
                                         value=get_template_names(plain=True)[0],
-                                    ).style(container=False)
+                                    )
                                 with gr.Column(scale=1):
                                     templateRefreshBtn = gr.Button("🔄 刷新")
                             with gr.Row():
@@ -155,7 +202,7 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                                         value=load_template(
                                             get_template_names(plain=True)[0], mode=1
                                         )[0],
-                                    ).style(container=False)
+                                    )
 
                 with gr.Tab(label="保存/加载"):
                     with gr.Accordion(label="保存/加载对话历史记录", open=True):
@@ -187,8 +234,8 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                                     downloadFile = gr.File(interactive=True)
 
                 with gr.Tab(label="高级"):
-                    gr.Markdown("# ⚠️ 务必谨慎更改 ⚠️\n\n如果无法使用请恢复默认设置")
                     default_btn = gr.Button("🔙 恢复默认设置")
+                    gr.Markdown("# ⚠️ 务必谨慎更改 ⚠️\n\n如果无法使用请恢复默认设置")
 
                     with gr.Accordion("参数", open=False):
                         top_p = gr.Slider(
@@ -208,88 +255,77 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
                             label="Temperature",
                         )
 
-                    with gr.Accordion("网络设置", open=False):
-                        apiurlTxt = gr.Textbox(
-                            show_label=True,
-                            placeholder=f"在这里输入API地址...",
-                            label="API地址",
-                            value="https://api.openai.com/v1/chat/completions",
-                            lines=2,
-                        )
-                        changeAPIURLBtn = gr.Button("🔄 切换API地址")
-                        proxyTxt = gr.Textbox(
-                            show_label=True,
-                            placeholder=f"在这里输入代理地址...",
-                            label="代理地址（示例：http://127.0.0.1:10809）",
-                            value="",
-                            lines=2,
-                        )
-                        changeProxyBtn = gr.Button("🔄 设置代理地址")
+                    apiurlTxt = gr.Textbox(
+                        show_label=True,
+                        placeholder=f"在这里输入API地址...",
+                        label="API地址",
+                        value="https://api.openai.com/v1/chat/completions",
+                        lines=2,
+                    )
+                    changeAPIURLBtn = gr.Button("🔄 切换API地址")
+                    proxyTxt = gr.Textbox(
+                        show_label=True,
+                        placeholder=f"在这里输入代理地址...",
+                        label="代理地址（示例：http://127.0.0.1:10809）",
+                        value="",
+                        lines=2,
+                    )
+                    changeProxyBtn = gr.Button("🔄 设置代理地址")
 
     gr.Markdown(description)
-    gr.HTML(footer.format(versions=versions_html()), elem_id="footer")
-    chatgpt_predict_args = dict(
-        fn=predict,
-        inputs=[
+
+    #keyTxt.submit(submit_key, keyTxt, [user_api_key, status_display])
+    #keyTxt.change(submit_key, keyTxt, [user_api_key, status_display])
+    # Chatbot
+    user_input.submit(
+        predict,
+        [
             user_api_key,
             systemPromptTxt,
             history,
-            user_question,
+            user_input,
             chatbot,
             token_count,
             top_p,
             temperature,
-            use_streaming_checkbox,
-            model_select_dropdown,
-            use_websearch_checkbox,
-            index_files,
-            language_select_dropdown,
+            use_streaming_checkbox
+            #model_select_dropdown,
+            #use_websearch_checkbox
+            #index_files
         ],
-        outputs=[chatbot, history, status_display, token_count],
+        [chatbot, history, status_display, token_count],
         show_progress=True,
     )
+    user_input.submit(reset_textbox, [], [user_input])
 
-    start_outputing_args = dict(
-        fn=start_outputing,
-        inputs=[],
-        outputs=[submitBtn, cancelBtn],
+    submitBtn.click(
+        predict,
+        [
+            user_api_key,
+            systemPromptTxt,
+            history,
+            user_input,
+            chatbot,
+            token_count,
+            top_p,
+            temperature,
+            use_streaming_checkbox
+            #model_select_dropdown,
+            #use_websearch_checkbox
+            #index_files
+        ],
+        [chatbot, history, status_display, token_count],
         show_progress=True,
     )
-
-    end_outputing_args = dict(
-        fn=end_outputing, inputs=[], outputs=[submitBtn, cancelBtn]
-    )
-
-    reset_textbox_args = dict(
-        fn=reset_textbox, inputs=[], outputs=[user_input]
-    )
-
-    transfer_input_args = dict(
-        fn=transfer_input, inputs=[user_input], outputs=[user_question, user_input, submitBtn, cancelBtn], show_progress=True
-    )
-
-    get_usage_args = dict(
-        fn=get_usage, inputs=[user_api_key], outputs=[usageTxt], show_progress=False
-    )
-
-
-    # Chatbot
-    cancelBtn.click(cancel_outputing, [], [])
-
-    user_input.submit(**transfer_input_args).then(**chatgpt_predict_args).then(**end_outputing_args)
-    user_input.submit(**get_usage_args)
-
-    submitBtn.click(**transfer_input_args).then(**chatgpt_predict_args).then(**end_outputing_args)
-    submitBtn.click(**get_usage_args)
+    submitBtn.click(reset_textbox, [], [user_input])
 
     emptyBtn.click(
         reset_state,
         outputs=[chatbot, history, token_count, status_display],
         show_progress=True,
     )
-    emptyBtn.click(**reset_textbox_args)
 
-    retryBtn.click(**start_outputing_args).then(
+    retryBtn.click(
         retry,
         [
             user_api_key,
@@ -299,19 +335,11 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
             token_count,
             top_p,
             temperature,
-            use_streaming_checkbox,
-            model_select_dropdown,
-            language_select_dropdown,
+            use_streaming_checkbox
+            #model_select_dropdown,
         ],
         [chatbot, history, status_display, token_count],
         show_progress=True,
-    ).then(**end_outputing_args)
-    retryBtn.click(**get_usage_args)
-
-    delFirstBtn.click(
-        delete_first_conversation,
-        [history, token_count],
-        [history, token_count, status_display],
     )
 
     delLastBtn.click(
@@ -331,18 +359,12 @@ with gr.Blocks(css=customCSS, theme=small_and_beautiful_theme) as demo:
             token_count,
             top_p,
             temperature,
-            gr.State(max_token_streaming//2 if use_streaming_checkbox.value else max_token_all//2),
-            model_select_dropdown,
-            language_select_dropdown,
+            use_streaming_checkbox
+            #model_select_dropdown,
         ],
         [chatbot, history, status_display, token_count],
         show_progress=True,
     )
-    reduceTokenBtn.click(**get_usage_args)
-
-    # ChatGPT
-    keyTxt.change(submit_key, keyTxt, [user_api_key, status_display]).then(**get_usage_args)
-    keyTxt.submit(**get_usage_args)
 
     # Template
     templateRefreshBtn.click(get_template_names, None, [templateFileSelectDropdown])
@@ -409,39 +431,23 @@ logging.info(
     + colorama.Style.RESET_ALL
 )
 # 默认开启本地服务器，默认可以直接从IP访问，默认不创建公开分享链接
-demo.title = "川虎ChatGPT 🚀"
+demo.title = "ChatGPT 3.5"
 
 if __name__ == "__main__":
-    reload_javascript()
     # if running in Docker
     if dockerflag:
         if authflag:
-            demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-                server_name="0.0.0.0",
-                server_port=7860,
-                auth=auth_list,
-                favicon_path="./assets/favicon.ico",
+            demo.queue().launch(
+                server_name="0.0.0.0", server_port=7860, auth=(username, password)
             )
         else:
-            demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-                server_name="0.0.0.0",
-                server_port=7860,
-                share=False,
-                favicon_path="./assets/favicon.ico",
-            )
+            demo.queue().launch(server_name="0.0.0.0", server_port=7860, share=False)
     # if not running in Docker
     else:
         if authflag:
-            demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-                share=False,
-                auth=auth_list,
-                favicon_path="./assets/favicon.ico",
-                inbrowser=True,
-            )
+            demo.queue().launch(share=False, auth=(username, password))
         else:
-            demo.queue(concurrency_count=CONCURRENT_COUNT).launch(
-                share=False, favicon_path="./assets/favicon.ico", inbrowser=True
-            )  # 改为 share=True 可以创建公开分享链接
-        # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", server_port=7860, share=False) # 可自定义端口
-        # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", server_port=7860,auth=("在这里填写用户名", "在这里填写密码")) # 可设置用户名与密码
-        # demo.queue(concurrency_count=CONCURRENT_COUNT).launch(auth=("在这里填写用户名", "在这里填写密码")) # 适合Nginx反向代理
+            demo.queue().launch(share=True)  # 改为 share=True 可以创建公开分享链接
+        # demo.queue().launch(server_name="0.0.0.0", server_port=7860, share=False) # 可自定义端口
+        # demo.queue().launch(server_name="0.0.0.0", server_port=7860,auth=("在这里填写用户名", "在这里填写密码")) # 可设置用户名与密码
+        # demo.queue().launch(auth=("在这里填写用户名", "在这里填写密码")) # 适合Nginx反向代理
